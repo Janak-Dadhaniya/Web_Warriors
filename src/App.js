fac904b26151045d5ae1047726fc069403d0a245
Janak-Dadhaniya <109734618+Janak-Dadhaniya@users.noreply.github.com>
2023-04-15 23:29:16 +0530

    second update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import Navbar from './components/navbar/navbar'
+
+import About from './components/About/about'
+import Connect from './components/Connect/connect'
+import HomePage from './components/homePage/homePage'
+import Cases from './components/selectedCases/cases'
+import Footer from './components/footer/footer'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <HomePage />
+      <About />
+      <Cases />
+      <Connect />
+      <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
